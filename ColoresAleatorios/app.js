2ade75ddae41526af322e77f1ca452e4631854a9
Fernando <ferweb.reyna@gmail.com>
2023-11-07 00:20:44 -0300

    Proyecto Colores hex aleatorio

---
+++ ColoresAleatorios/app.js
@@ -0,0 +1,27 @@
+// Seleccionar los elementos del DOM.
+const boton = document.querySelector('button');
+const color = document.getElementById('color');
+
+// Generar un color hexadecimal aleatorio.
+function generarColorHexAleatorio() {
+  let digitos = '0123456789ABCDEF'; // 16 opciones.
+  let color = '#'; // El color inicia con #.
+
+  for (let i = 0; i < 6; i++) {
+    // Escoger uno de los 16 digitos posibles.
+    let indiceAleatorio = Math.floor(Math.random() * 16);
+    // Agregarlo al codigo del color.
+    color += digitos[indiceAleatorio];
+  }
+  
+  return color;
+}
+
+// Event Listener.
+boton.addEventListener('click', function() {
+  let colorAleatorio = generarColorHexAleatorio();
+  // actualizar color de fondo
+  document.body.style.backgroundColor = colorAleatorio;
+  // actualizar texto.
+  color.textContent = colorAleatorio;
+});
